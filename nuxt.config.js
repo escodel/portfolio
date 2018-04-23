692038fbbe6a232dc86180daa42da685b9bb5095
escodel <escodel@gmail.com>
2018-04-23 15:48:13 -0400

    Twitter card on main page

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,8 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Eric Delia is a senior front-end developer with a passion for HTML, CSS, and JavaScript. This portfolio site was built with Vue, Nuxt, Bulma, and Storyblok.' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:creator', name: 'twitter:creator', content: '@ericdelia_' },
       { hid: 'site-verification', name:'google-site-verification', content: '5Okin1cAGo23N6WtAww_8qbIBq5YdcNjYRwNEErG-RY' }
     ],
     link: [
