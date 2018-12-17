3d54d24fc06bfcf5a49791ea937dbd9f58106f15
Eric Delia <escodel@gmail.com>
2018-12-16 23:30:31 -0500

    Working on styling and content

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 (() => {
   let navTrigger = document.querySelector('.nav-trigger');
   let navMenu = document.querySelector('.nav-list');
+  let content = document.querySelector('main');
+  let aboutSection = document.querySelector('.about-section');
+  let about = document.querySelector('.about');
+  let projectsSection = document.querySelector('.projects-section');
+  let projects = document.querySelector('.projects');
+
+  let isInViewPort = elem => {
+    var bounding = elem.getBoundingClientRect();
+    return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <=
+        (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
 
   let openNav = () => {
     if (navMenu.classList.contains('slideInDown')) {
@@ -13,4 +30,16 @@
   };
 
   navTrigger.addEventListener('click', openNav);
+  content.addEventListener(
+    'scroll',
+    () => {
+      if (isInViewPort(aboutSection)) {
+        about.classList.add('animated', 'slideInRight', 'slow');
+      }
+      if (isInViewPort(projectsSection)) {
+        projects.classList.add('animated', 'slideInLeft', 'slow');
+      }
+    },
+    false
+  );
 })();
